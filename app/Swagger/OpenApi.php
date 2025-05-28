cfea696bdf18b4728fbacf3ba082441702f5a0ed
alexOrduL <aordunol95@gmail.com>
2025-05-28 04:44:48 -0600

    feat(documentation): Added swager OpenApi documentation

---
+++ app/Swagger/OpenApi.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Swagger;
+
+/**
+ * @OA\Info(
+ *     title="Mini CRM API",
+ *     version="1.0.0",
+ *     description="A versioned REST API for managing companies, contacts, and deals"
+ * )
+ * 
+ * @OA\Server(
+ *     url=L5_SWAGGER_CONST_HOST,
+ *     description="Localhost server"
+ * )
+ */
+class OpenApi {}
