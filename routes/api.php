41be2c126982bb4e8a0f611710dbd3f36a06761e
alexOrduL <aordunol95@gmail.com>
2025-05-28 00:54:49 -0600

    feat(api): implement versioned routing structure with /api/v1 prefix

---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\Api\V1\CompanyController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\ContactController;
+use App\Http\Controllers\Api\V1\DealController;
+
+Route::prefix('v1')->group(function () {
+    Route::get('companies', [CompanyController::class, 'show']);
+    Route::post('companies', [CompanyController::class, 'store']);
+    Route::put('companies/{company}', [CompanyController::class, 'store']);
+    Route::delete('companies/{company}', [CompanyController::class, 'destroy']);
+    
+    Route::get('contacts', [ContactController::class, 'show']);
+    Route::post('companies/{companyId}/contacts', [CompanyController::class, 'attachContact']);
+    Route::put('contacts/{contact}', [ContactController::class, 'update']);
+    Route::delete('contacts/{contact}', [ContactController::class, 'destroy']);
+
+    
+    Route::get('/deals', [DealController::class, 'show']);
+    Route::post('/deals', [DealController::class, 'store']);
+    Route::put('/deals/{deal}', [DealController::class, 'update']);
+    Route::delete('/deals/{deal}', [DealController::class, 'destroy']);
+
+});
